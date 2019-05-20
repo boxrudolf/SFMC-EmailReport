3d68f7a5be4d33c82ed9b492e00d96fe71f7ebeb
boxrudolf <boxrudolf@gmail.com>
2019-05-20 12:04:32 +0200

    d

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,13 @@ var express = require('express');
 var router = express.Router();
 var jwt = require('jwt-simple');
 var url = require('url');
-const log = require('simple-node-logger').createSimpleLogger('project.log');
 
-log.info("Inicio Entra en la app");
 
 /* GET home page. */
 router.get('/login', function (request, response, next) {
+    const log = require('simple-node-logger').createSimpleLogger('project.log');
+    log.info("Inicio Entra en la app");
+
     var url_parts = url.parse(request.url, true);
     var query = url_parts.query;
 
@@ -21,6 +22,9 @@ router.get('/login', function (request, response, next) {
 });
 
 router.get('/logout', function (request, response, next) {
+    const log = require('simple-node-logger').createSimpleLogger('project.log');
+    log.info("Inicio Entra en la app");
+
     var url_parts = url.parse(request.url, true);
     var query = url_parts.query;
 
@@ -33,13 +37,16 @@ router.get('/logout', function (request, response, next) {
 });
 
 router.get('/', function (request, response, next) {
+    const log = require('simple-node-logger').createSimpleLogger('project.log');
+    log.info("Inicio Entra en la app");
+
     var url_parts = url.parse(request.url, true);
     var query = url_parts.query;
-    log.info("LOGOUT");
+    log.info("Raiz");
     log.info("Headers" + JSON.stringify(request.headers));
     log.info("UrlVars:" + JSON.stringify(query));
     log.info("Body" + JSON.stringify(request.body));
-    response.render('/', { title: "TOKEN " + JSON.stringify("Notoken")});    
+    response.render('index', { title: "TOKEN " + JSON.stringify("Notoken")});    
 });
 
 module.exports = router;
