8efc0d2dbd5028d816d8bb28f0ca14805e8137a8
boxrudolf <boxrudolf@gmail.com>
2019-05-20 14:39:34 +0200

    Api

--- routes/index.js
+++ routes/index.js
@@ -15,9 +15,9 @@ router.get('/login', function (request, response, next) {
     log.info("LOGIN Updated");
     log.info("Headers" + JSON.stringify(request.headers));
     log.info("UrlVars:" + JSON.stringify(query));
-    log.info("Body" + JSON.stringify(request.body.length));
-    log.info("Cookie" + JSON.stringify(request.cookies.length));
-    log.info("Params" + JSON.stringify(request.params.length));
+    log.info("Body" + request.body);
+    log.info("Cookie" + request.cookies);
+    log.info("Params" + request.params);
 
     response.render('index', { title: "POST LOGIN" }); 
 
