1eaac3a88d38ae0f5ec48c5e7d49efdfe727c49f
boxrudolf <boxrudolf@gmail.com>
2019-05-20 11:37:49 +0200

    logger

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var jwt = require('jwt-simple');
+var url = require('url');
+const log = require('simple-node-logger').createSimpleLogger('project.log');
+
+log.info("Inicio Entra en la app");
+
+
+
 
 
 // CODIFICADOR JWT
@@ -18,10 +25,19 @@ var secret = "XwdKtoBAV9fwBfFKCZCmTyCW";
 var decodedJWT = jwt.decode(encodedJWT, secret);
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', function (request, response, next) {
+    var url_parts = url.parse(request.url, true);
+    var query = url_parts.query;
+    console.log("--------------");
+ 
 
-  res.render('index', {title: "TOKEN "+ JSON.stringify(token)+"  DECODEDJWT  "+ JSON.stringify(decodedJWT)})
-
+    log.info("Headers" + JSON.stringify(request.headers));
+    log.info("UrlVars:" + JSON.stringify(query));
+    log.info("Body" + JSON.stringify(request.body));
+    
+    
+    
+    response.render('index', { title: "TOKEN " + JSON.stringify(token) + "  DECODEDJWT  " + JSON.stringify(decodedJWT) });
   // res.render('index', {
   //   title: JSON.stringify(token) + "||" + JSON.stringify(decoded) +
   //     "||" + JSON.stringify(payload)
