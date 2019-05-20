b1bd01a98037580230494b789e1598a8cc444e4b
boxrudolf <boxrudolf@gmail.com>
2019-05-20 12:35:17 +0200

    a

--- routes/index.js
+++ routes/index.js
@@ -16,8 +16,27 @@ router.get('/login', function (request, response, next) {
     log.info("Headers" + JSON.stringify(request.headers));
     log.info("UrlVars:" + JSON.stringify(query));
     log.info("Body" + JSON.stringify(request.body));
+    log.info("Params" + JSON.stringify(request.params));
            
-    response.render('index', { title: "LOGIN"});
+    response.render('index', { title: "GET LOGIN"});
+
+});
+
+/* GET home page. */
+router.post('/login', function (request, response, next) {
+    const log = require('simple-node-logger').createSimpleLogger('project.log');
+    log.info("Inicio Entra en la app");
+
+    var url_parts = url.parse(request.url, true);
+    var query = url_parts.query;
+
+    log.info("LOGIN");
+    log.info("Headers" + JSON.stringify(request.headers));
+    log.info("UrlVars:" + JSON.stringify(query));
+    log.info("Body" + JSON.stringify(request.body));
+    log.info("Params" + JSON.stringify(request.params));
+
+    response.render('index', { title: "POST LOGIN" });
 
 });
 
